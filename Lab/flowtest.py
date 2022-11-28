35648f0683e5118ff4be6bf51b2ac55a7d01a615
charlesember <wurstfightele@gmail.com>
2022-11-28 11:03:39 +0100

    formatting

--- Lab/flowtest.py
+++ Lab/flowtest.py
@@ -193,8 +193,8 @@ for info in dataheader:
         i+=1
 plt.show()
 
-
-
+"""Adding the Classification"""
+#for 1400rpm
 y = Data[Data["test_info"]==1400]["flap_pos_open"]
 X=Data[Data["test_info"]==1400][["peak_bladepass","a_rms"]]
 Xtrain, Xtest, ytrain, ytest = train_test_split(X,y, test_size=0.25, random_state=0)        
@@ -204,7 +204,7 @@ ypred = model.predict(Xtrain)
 correct = accuracy_score(ytrain,ypred, normalize=False)
 incorrect = np.size(ytrain)-correct
 print(f"1400rpm-peak_bladepass/a_rms: Score=W{incorrect}-R{correct} accuracy={accuracy_score(ytrain,ypred):.2f}%")
-
+#for 2800rpm
 y = Data[Data["test_info"]==2800]["flap_pos_open"]
 X=Data[Data["test_info"]==2800][["peak_bladepass","a_rms"]]
 Xtrain, Xtest, ytrain, ytest = train_test_split(X,y, test_size=0.25, random_state=0)        
